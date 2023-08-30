ecdb965989fd185fe89dd8e60389d9b4272eedb0
JitHoon <imessi0336@gmail.com>
2023-08-30 16:11:31 +0900

    Function 컴포넌트로 사용하기 (React Hooks)

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,53 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Square } from "./Square";
 import "./Board.css";
 
-export class Board extends Component {
+export const Board = () => {
   // 공유 state
-  constructor(props) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-    };
-  }
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
   // Squar 자녀에게 물려 줄 click handle 함수
-  setX(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = "X";
-    this.setState({ squares: squares });
-  }
+  const setX = (i) => {
+    const newSquares = squares.slice();
+    newSquares[i] = "X";
+    setSquares(newSquares);
+  };
 
-  renderSquare(i) {
+  const renderSquare = (i) => {
     return (
       <Square
-        value={this.state.squares[i]}
         //  Squar 자녀에게 props 형태로 함수 전달
-        handleClick={() => this.setX(i)}
+        value={squares[i]}
+        handleClick={() => setX(i)}
       />
     );
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <div className="status">Next Player: X, O</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+  return (
+    <div>
+      <div className="status">Next Player: X, O</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-    );
-  }
-}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  );
+};
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,10 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Square.css";
 
-export class Square extends Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.handleClick() /* props 함수 가져오기*/}
-      >
-        {this.props.value /* state 접근 */}
-      </button>
-    );
-  }
-}
+export const Square = ({ value, handleClick }) => {
+  return (
+    <button className="square" onClick={handleClick /* props 함수 가져오기*/}>
+      {value /* props 접근 */}
+    </button>
+  );
+};
