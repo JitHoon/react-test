522be0ddb5f88206527f83ecbb3be648bc12d6dd
JitHoon <imessi0336@gmail.com>
2023-08-30 10:50:59 +0900

    App.js 컴포넌트 세팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hallo</div>;
+  return (
+    <div className="game">
+      <div className="game-board">Hallo!</div>
+      <div className="game-info"></div>
+    </div>
+  );
 }
 
 export default App;
