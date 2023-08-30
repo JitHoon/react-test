47756c385cd299deba42e372603e6ff2b70725b8
JitHoon <imessi0336@gmail.com>
2023-08-30 14:24:12 +0900

    this.state + setState()와 props로 공유 state 만들기

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,8 +3,29 @@ import { Square } from "./Square";
 import "./Board.css";
 
 export class Board extends Component {
+  // 공유 state
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  // Squar 자녀에게 물려 줄 click handle 함수
+  setX(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
   renderSquare(i) {
-    return <Square numberProps={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        //  Squar 자녀에게 props 형태로 함수 전달
+        handleClick={() => this.setX(i)}
+      />
+    );
   }
 
   render() {
--- src/components/Square.js
+++ src/components/Square.js
@@ -2,22 +2,13 @@ import React, { Component } from "react";
 import "./Square.css";
 
 export class Square extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numberProps: null,
-    };
-  }
   render() {
     return (
       <button
         className="square"
-        onClick={() => {
-          /* state settet */
-          this.setState({ numberProps: "X" });
-        }}
+        onClick={() => this.props.handleClick() /* props 함수 가져오기*/}
       >
-        {this.state.numberProps /* state 접근 */}
+        {this.props.value /* state 접근 */}
       </button>
     );
   }
