ed530cabef9824b2c2c21a8c3d111c654a94d430
JitHoon <imessi0336@gmail.com>
2023-08-30 11:21:43 +0900

    Import Board 컴포넌트 in App 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Board } from "./components/Board";
 
 function App() {
   return (
     <div className="game">
-      <div className="game-board">Hallo!</div>
+      <div className="game-board">
+        <Board />
+      </div>
       <div className="game-info"></div>
     </div>
   );
